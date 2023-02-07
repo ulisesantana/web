c4356e89af8f3beac2528bfb66211040d9f8924e
Ulises Santana <ulisesantana@gmail.com>
2023-02-07 21:40:00 +0000

    ✨ add scroll to top button

--- src/components/molecules/scroll-to-top.js
+++ src/components/molecules/scroll-to-top.js
@@ -1,82 +1,39 @@
-import { supportsPassiveEvents } from 'detect-passive-events'
-import TweenFunctions from 'tween-functions'
-
 class ScrollToTop extends HTMLElement {
   show = false
-  showUnder = 300
-  component = null
-  data = {
-    startValue: 0,
-    currentTime: 0, // store current time of animation
-    startTime: null,
-    rafId: null
-  }
+  element = null
 
   connectedCallback () {
-    this.component = this.querySelector(':scope .scroll-to-top')
+    this.element = this.querySelector(':scope .scroll-to-top')
     this.handleScroll()
-
     // Add all listeners which can start scroll
-    this.component.addEventListener('click', this.handleClick)
-    window.addEventListener('scroll', this.handleScroll)
-    window.addEventListener('wheel', this.stopScrolling, supportsPassiveEvents ? { passive: true } : false)
-    window.addEventListener('touchstart', this.stopScrolling, supportsPassiveEvents ? { passive: true } : false)
+    this.element.addEventListener('click', this.handleClick.bind(this))
+    window.addEventListener('scroll', this.handleScroll.bind(this))
   }
 
   disconnectedCallback () {
-    this.component.removeEventListener('click', this.handleClick)
+    this.element.removeEventListener('click', this.handleClick)
     window.removeEventListener('scroll', this.handleScroll)
-    window.removeEventListener('wheel', this.stopScrolling, false)
-    window.removeEventListener('touchstart', this.stopScrolling, false)
   }
 
   handleScroll () {
-    this.show = window.pageYOffset > this.showUnder
+    const scrollLimit = 300
+    if (window.pageYOffset > scrollLimit && !this.show) {
+      this.show = true
+      this.element.classList.remove('hidden')
+    } else if (window.pageYOffset <= scrollLimit) {
+      this.show = false
+      this.element.classList.add('hidden')
+    }
   }
 
   /**
    * Handle click on the button
    */
   handleClick () {
-    this.stopScrolling()
-    this.data.startValue = window.pageYOffset
-    this.data.currentTime = 0
-    this.data.startTime = null
-    this.data.rafId = window.requestAnimationFrame(this.scrollStep)
-  }
-
-  /**
- * Calculate new position
- * and scroll screen to new position or stop scrolling
- * @param timestamp
- */
-  scrollStep (timestamp) {
-    if (!this.data.startTime) {
-      this.data.startTime = timestamp
-    }
-
-    this.data.currentTime = timestamp - this.data.startTime
-
-    const position = TweenFunctions[this.props.easing](
-      this.data.currentTime,
-      this.data.startValue,
-      this.props.topPosition,
-      this.props.duration
-    )
-
-    if (window.pageYOffset <= this.props.topPosition) {
-      this.stopScrolling()
-    } else {
-      window.scrollTo(window.pageYOffset, position)
-      this.data.rafId = window.requestAnimationFrame(this.scrollStep)
-    }
-  }
-
-  /**
-   * Stop Animation Frame
-   */
-  stopScrolling () {
-    window.cancelAnimationFrame(this.data.rafId)
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    })
   }
 }
 
