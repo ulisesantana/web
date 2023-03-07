84a423df04881f85980e7b6d89d659808164da02
Ulises Santana <ulisesantana@gmail.com>
2023-03-07 13:39:31 +0000

    👷‍♂️ fix legacy build

--- scripts/build-legacy.sh
+++ scripts/build-legacy.sh
@@ -1,24 +1,10 @@
 #/usr/bin/bash
 
-dev=false
 
-while getopts ":d" opt; do
-  case $opt in
-    d) # flag -d
-      dev=true
-      ;;
-    \?) # if any other flag
-      echo "Invalid option: -$OPTARG" >&2
-      exit 1
-      ;;
-  esac
-done
 
 cd ./src/legacy
-if test -z "$dev"; then
-  echo "📦 Installing legacy dependencies."
-  npm install
-fi
+echo "📦 Installing legacy dependencies."
+npm install
 echo "🚧 Starting build."
 npm run build
 echo "👷 Build done."
