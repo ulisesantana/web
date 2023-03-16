c1d9fe2be2af58c4d87bd42617b89b0b516ae6e7
ulisesantana <ulisesantana@users.noreply.github.com>
2023-03-16 20:41:35 +0000

    deploy: 8f55b37d91685110c51813cb9ba6edf99e43f4f5

--- components/youtube-video.js
+++ components/youtube-video.js
@@ -1,6 +1,13 @@
 const youTubeVideoStyle = /* html */`
 <style>
-  .video-wrapper {
+  .youtube-video {
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+
+  .youtube-video .video-wrapper {
     position: relative;
     overflow: hidden;
     width: 100%;
@@ -9,7 +16,7 @@ const youTubeVideoStyle = /* html */`
   }
 
   /* Then style the iframe to fit in the container div with full height and width */
-  .video-wrapper iframe {
+  .youtube-video .video-wrapper iframe {
     border-radius: var(--border-radius);
     position: absolute;
     top: 0;
@@ -23,19 +30,23 @@ const youTubeVideoStyle = /* html */`
 `
 
 class YouTubeVideo extends HTMLElement {
-  constructor () {
+  constructor() {
     super()
     this.attachShadow({ mode: 'open' })
   }
 
-  connectedCallback () {
+  connectedCallback() {
     const videoId = this.getAttribute('video-id')
+    const caption = this.getAttribute('video-caption') || ''
     this.shadowRoot.innerHTML = /* html */`
     ${youTubeVideoStyle}
-    <div class="video-wrapper">
-      <iframe src="https://www.youtube-nocookie.com/embed/${videoId}" title="YouTube video player" frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen></iframe>
+    <div class="youtube-video">
+      <div class="video-wrapper">
+        <iframe src="https://www.youtube-nocookie.com/embed/${videoId}" title="YouTube video player" frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen></iframe>
+      </div>
+      <small>${caption}</small>
     </div>
     `
   }
