6a3ea5bc2a5cd31a4da4e268b8e7a75e13be8b7d
Ulises Santana <ulisesantana@gmail.com>
2025-06-21 09:45:55 +0100

    🔧 change spanish menu order

--- src/data/nav.js
+++ src/data/nav.js
@@ -1,15 +1,15 @@
 module.exports = {
   es: [
     { url: '/', label: '🇬🇧' },
-    { url: '/es/blog/', label: 'blog' },
     { url: '/es/', label: 'sobre mí' },
+    { url: '/es/blog/', label: 'blog' }
     // { url: '/proyectos/', label: 'proyectos' },
     // { url: '/notas/', label: 'notas' }
   ],
   en: [
     { url: '/es/', label: '🇪🇸' },
     { url: '/', label: 'about me' },
-    { url: '/blog/', label: 'blog' },
+    { url: '/blog/', label: 'blog' }
     // { url: '/en/projects/', label: 'projects' }
   ]
 }
