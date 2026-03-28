56a591db260c31a306644a8d0ceca742b11379f2
Ulises Santana <ulisesantana@gmail.com>
2026-03-28 12:42:54 +0000

    feat: update default SEO image

--- src/config.ts
+++ src/config.ts
@@ -4,18 +4,13 @@ export const SITE = {
   profile: "https://ulisesantana.dev/",
   desc: "Full Stack Developer with JavaScript as mother tongue and web as my home nation. Based in the Canary Islands 🏝️.",
   title: "Ulises Santana",
-  ogImage: "astropaper-og.jpg",
+  ogImage: "favicon.png", // default image for meta tags
   lightAndDarkMode: true,
   postPerIndex: 4,
   postPerPage: 10,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
   showArchives: false,
   showBackButton: true, // show back button in post detail
-  editPost: {
-    enabled: false,
-    text: "Suggest Changes",
-    url: "https://github.com/satnaing/astro-paper/edit/main/",
-  },
   dynamicOgImage: true,
   lang: "en", // html lang code. Set this empty and default will be "en"
   timezone: "Europe/London", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
